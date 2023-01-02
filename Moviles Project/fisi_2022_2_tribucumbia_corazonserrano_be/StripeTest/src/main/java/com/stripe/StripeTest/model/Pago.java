11372657e89982d8d3eb2a4cf2e6d0796c051a04
Ruben Arce <99149208+Rubac19@users.noreply.github.com>
2023-01-02 00:06:32 -0500

    Add files via upload

---
+++ Moviles Project/fisi_2022_2_tribucumbia_corazonserrano_be/StripeTest/src/main/java/com/stripe/StripeTest/model/Pago.java
@@ -0,0 +1,66 @@
+package com.stripe.StripeTest.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pago")
+public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pago", nullable = false)
+    private Integer id;
+
+    @Column(name = "fecha", nullable = false)
+    private LocalDate fecha;
+
+    @Column(name = "monto", nullable = false)
+    private Double monto;
+
+    @Column(name = "nombreArchivo", nullable = false)
+    private String archivo;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "id_matricula", nullable = false)
+    private Matricula idMatricula;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public Matricula getIdMatricula() {
+        return idMatricula;
+    }
+
+    public void setIdMatricula(Matricula idMatricula) {
+        this.idMatricula = idMatricula;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+}
